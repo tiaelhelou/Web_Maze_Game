b38a210025ec888b13e22f44b6524f1ac714f0b1
tiahelou10 <tia.elhelou@outlook.com>
2022-09-18 00:24:02 +0300

    fixed reset function

--- game.js
+++ game.js
@@ -1,15 +1,15 @@
 window.onload = () => {
 
     const element = document.getElementsByClassName("boundary");
-
-    
+ 
     document.getElementById("start").addEventListener("click", reset);
 
     function reset() {
         for (let i = 0; i < element.length; i++) {
-            element[i].style.background = "black";
+            element[i].style.background = "#eeeeee";
 
         }
+        document.getElementById("status").innerHTML = "Begin by moving your mouse over the S.";
     }
 
 }
